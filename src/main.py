78b1ef231c455f1e6349fd83d6a20ec00d2e0838
horse-3903 <chongchoonhourafael@gmail.com>
2024-10-16 13:18:02 +0800

    v0.1.2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
+import os
+
 import cv2
 from ultralytics import YOLO
-import os
 
 # Load the YOLOv8 model trained for pose detection (e.g., face, hands, body landmarks)
 print("Loading Model...", end="")
@@ -13,14 +14,18 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 # Open the video file (you can also use 0 for the default webcam)
-cap = cv2.VideoCapture("./data/test.mp4")
+file_name = "test.mp4"
+cap = cv2.VideoCapture(f"./data/{file_name}")
 
 # Get video dimensions (width and height)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 # Set up VideoWriter to save the output video
-output_path = f'{output_dir}/test-output-2.mp4'  # Use the correct relative path
+all_output_files = os.listdir(output_dir)
+past_output_files = [*filter(lambda file: file_name.split(".")[0] in file, all_output_files)]
+
+output_path = f'{output_dir}/test-output-{len(past_output_files)+1}.mp4'  # Use the correct relative path
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # Codec for mp4 format
 fps = int(cap.get(cv2.CAP_PROP_FPS)) 
 out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
@@ -35,11 +40,10 @@ while True:
     ret, frame = cap.read()
 
     if not ret:
-        print("Error: Could not read frame from video")
         break
 
     # Run the YOLO model on the frame for pose detection (face, hands, body)
-    results = model.predict(source=frame, conf=0.25, max_det=10, verbose=True)
+    results = model.predict(source=frame, conf=0.3, max_det=15, verbose=True)
 
     # Annotate the frame with detected landmarks, bounding boxes, and labels
     annotated_frame = results[0].plot()
@@ -53,7 +57,7 @@ while True:
     cv2.imshow('YOLOv11 Pose Detection', annotated_frame)
 
     # Break the loop if the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
 # Release the video capture and the video writer, and close the display window
---
+++ src/util.py
@@ -0,0 +1,79 @@
+import os
+from pathlib import Path
+
+from tqdm import tqdm
+
+import cv2
+from ultralytics import YOLO
+
+class ModelNotFoundError(Exception):
+    pass
+
+class SprintTracker:
+    def __init__(self, model, input_file, output_file):
+        if model in [model.split(".")[0] for model in os.listdir("models")]:
+            raise ModelNotFoundError(f"Model {model} not found, please include the parent directory 'model/[model-name]' if you have not")
+
+        print("Loading Model...", end="")
+        self.model = YOLO(model)
+        print("Done")
+
+        if not os.path.exists(os.path.abspath(input_file)):
+            raise FileNotFoundError(f"Input file {os.path.abspath(input_file)} not found")
+        
+        output_path = Path(output_file)
+
+        if not os.path.exists(os.path.abspath(output_path.parent.absolute())):
+            raise FileNotFoundError(f"Output file {output_path.parent.absolute()} not found")
+
+        self.input_file = input_file
+        self.output_file = output_file
+
+    def track_sprint(self, conf: float = 0.5, max_det: int = 1, verbose: bool = False, save: bool = False):
+        cap = cv2.VideoCapture(self.input_file)
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+        if save:
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            fps = int(cap.get(cv2.CAP_PROP_FPS)) 
+            out = cv2.VideoWriter(self.output_file, fourcc, fps, (width, height))
+        
+        if not cap.isOpened():
+            print("Error: Could not open video")
+            exit()
+
+        for frame in tqdm(range(frame_count)):
+            ret, frame = cap.read()
+
+            if not ret:
+                break
+
+            results = self.model.predict(source=frame, conf=conf, max_det=max_det, verbose=verbose)
+
+            annotated_frame = results[0].plot()
+
+            if save:
+                out.write(annotated_frame)
+
+            cv2.namedWindow('YOLOv11 Pose Detection', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('YOLOv11 Pose Detection', width, height)
+            cv2.imshow('YOLOv11 Pose Detection', annotated_frame)
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+
+        if save:
+            out.release()
+
+        cv2.destroyAllWindows()
+
+        print(f"Output video saved to {self.output_file}")
+        
+if __name__ == "__main__":
+    st = SprintTracker(model="models/yolo11x-pose.pt", input_file="input/test.mp4", output_file="output/test-output-4.mp4")
+    st.track_sprint(conf=0.5, max_det=10, verbose=False, save=True)
